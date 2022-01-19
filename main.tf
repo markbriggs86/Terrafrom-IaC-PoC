585d449a08ef5a0c1e2d0633c160ee95c3eafed2
Mark Briggs <mza@DUBLIN.MAPLESANDCALDER.com>
2022-01-19 14:30:58 +0000

    subnet in main.tf and closing bracket

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ terraform {
       name = "Terraform-IaC-PoC"
     }
   }
-  
+}  
 provider "azurerm" {
  features {}
 }
@@ -38,4 +38,11 @@ resource "azurerm_virtual_network" "MarksOrg-vnet" {
   address_space       = ["10.0.0.0/16"]
   location            = "uksouth"
   resource_group_name = azurerm_resource_group.rg.name
+}
+#subnet
+resource "azurerm_subnet" "subnet1" {
+  name                 = "Subnet1"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.MarksOrg-vnet.name
+  address_prefix       = "10.0.1.0/24"
 }
