b45757afec5d963899663f6ed483b008f8d604c7
Mark Briggs <mza@DUBLIN.MAPLESANDCALDER.com>
2022-01-20 06:42:25 +0000

    publicIP added

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
       version = "~> 2.65"
     }
   }
-	
+
   cloud {
     organization = "MarksOrg"
 
@@ -15,9 +15,9 @@ terraform {
       name = "Terraform-IaC-PoC"
     }
   }
-}  
+}
 provider "azurerm" {
- features {}
+  features {}
 }
 
 #Create Resource Group
@@ -27,11 +27,11 @@ resource "azurerm_resource_group" "rg" {
 
   tags = {
     Environment = "MarksOrg-Testing"
-	Workspace = "Terraform-IaC-PoC"
-	Repository = "GitHub"
+    Workspace   = "Terraform-IaC-PoC"
+    Repository  = "GitHub"
   }
 }
-	
+
 #Define vnet and subnet
 resource "azurerm_virtual_network" "MarksOrg-vnet" {
   name                = "MarksOrg-vnet"
@@ -45,4 +45,12 @@ resource "azurerm_subnet" "subnet1" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.MarksOrg-vnet.name
   address_prefix       = "10.0.1.0/24"
+}
+#Define PublicIP Address
+resource "azurerm_public_ip" "myvm1publicip" {
+  name                = "pip1"
+  location            = "uksouth"
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+  sku                 = "Basic"
 }
